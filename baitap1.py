cbad3850ba96a1dc1b8d5d438d40a110aa9af589
hoangcaobao <caobaohoang03@yahoo.com.vn>
2020-05-10 23:53:19 +0700

    dasualoi

--- baitap1.py
+++ baitap1.py
@@ -1,5 +1,5 @@
-a=[1,3,4,16,32,8,64,4,128,2,256,32]
-
+# a=[1,3,4,16,32,8,64,4,128,2,256,32]
+a=[16,2,4,2,128,64,7,1,64,32,16,5,8]
 def timmax(b):
     i=b[0];
     for j in range(1,len(b)):
@@ -14,7 +14,7 @@ for i in range(timmax(a)+1):
 
 for i in range(0,len(a)-1):
     for j in range(0, len(a)):
-        if a[i]*a[j]==256 and dadung[a[i]]==False and dadung[a[j]]==False :
+        if a[i]*a[j]==256 and dadung[a[i]]==False and dadung[a[j]]==False and i != j:
             print(a[i],a[j],i,j)
             dadung[a[i]]=True
             dadung[a[j]]=True
